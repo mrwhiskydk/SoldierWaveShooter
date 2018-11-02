8fa6030e2c464b53871338d3f03efee1ecefd6ec
Morten Bay Skov <mort79e5@student.eadania.dk>
2018-11-02 11:51:55 +0100

    dadada

--- SoldierWaveShooter/SoldierWaveShooter/GameObjects.cs
+++ SoldierWaveShooter/SoldierWaveShooter/GameObjects.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,11 @@ namespace SoldierWaveShooter
 {
     public class GameObjects
     {
+        protected Texture2D sprite;
+        public Vector2 position;
+        protected float rotation;
+        
+
 
     }
 }
