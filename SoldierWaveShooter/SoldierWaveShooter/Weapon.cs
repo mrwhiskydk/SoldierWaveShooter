6895576c7308f5ce702687fdcf553fa7d06d970f
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2018-11-02 11:42:02 +0100

    dgh

---
+++ SoldierWaveShooter/SoldierWaveShooter/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoldierWaveShooter
+{
+    public class Weapon
+    {
+        protected int ammo;
+        protected float firerate;
+        protected float projectilespeed;
+        protected int damage;
+    }
+}
