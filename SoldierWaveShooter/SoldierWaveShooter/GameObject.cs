30b680ed0aef46957038de28f158effcdbc8e190
Morten Bay Skov <mort79e5@student.eadania.dk>
2018-11-02 12:01:39 +0100

    dadada gameobject class

--- SoldierWaveShooter/SoldierWaveShooter/GameObject.cs
+++ SoldierWaveShooter/SoldierWaveShooter/GameObject.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,17 @@ namespace SoldierWaveShooter
 {
     public class GameObject
     {
+        protected Texture2D sprite;
+        protected float rotation;
+
+        public Vector2 position;
+
+        /// <summary>
+        /// Property that gets the position of GameObject
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        public Rectangle CollisionBox { get; set; }
 
     }
 }
