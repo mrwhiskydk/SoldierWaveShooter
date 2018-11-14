dd0142080fe7058f0b90dffe8c06a15a6ab56d1e
mrwhiskydk <sein@live.dk>
2018-11-14 15:06:13 +0100

    dsafwef

--- SoldierWaveShooter/SoldierWaveShooter/BulletCasing.cs
+++ SoldierWaveShooter/SoldierWaveShooter/BulletCasing.cs
@@ -44,7 +44,7 @@ namespace SoldierWaveShooter
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(sprite, position, null, Color.White, rotation, new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f), 1f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(sprite, position, null, Color.White, rotation, new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f), 1f, SpriteEffects.None, 0.85f);
         }
 
         public override void DoCollision(GameObject otherObject)
